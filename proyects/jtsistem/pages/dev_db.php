5df8ca397a797167f61ef58a72186a1c24946841
JoseTsPlcs <jalonsovp@gmail.com>
2024-05-23 08:47:24 -0500

    error dev_db

--- proyects/jtsistem/pages/dev_db.php
+++ proyects/jtsistem/pages/dev_db.php
@@ -20,6 +20,7 @@ $conn = new mysqli($server, $username, $password, $database);
 // Verificar la conexión
 if ($conn->connect_error) {
     die("Error de conexión: " . $conn->connect_error);
+    echo $conn->connect_error;
 }
 
 echo "Conexión exitosa a la base de datos";
