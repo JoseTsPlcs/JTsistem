429bb1c2e0db5ee733f22be4c49c8cf91a014b4f
JoseTsPlcs <jalonsovp@gmail.com>
2024-08-21 16:33:35 -0500

    index

--- aaa_css.php
+++ aaa_css.php
@@ -1,6 +1,8 @@
 <?php
 
-    $srcPathReturn = "../../../";
+    if (!isset($srcPathReturn)) {
+        $srcPathReturn = '../../../';
+    }
 
     echo '<meta charset="utf-8">';
     echo '<meta name="viewport" content="width=device-width, initial-scale=1.0">';
@@ -15,8 +17,8 @@
     echo '<link href="https://cdn.lineicons.com/4.0/lineicons.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../../style.css">';
+    <link rel="stylesheet" href="'.$srcPathReturn.'style.css">';
 
-    echo '<link rel="stylesheet" href="../../../tutorialStyle.css">';
+    echo '<link rel="stylesheet" href="'.$srcPathReturn.'tutorialStyle.css">';
    
 ?>
